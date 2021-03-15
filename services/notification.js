e7062d4d88aee146f18c61e51d06862eb4bac724
Ievgenii Butyrin <iebutyrin@deloitte.com>
2021-03-15 18:11:53 +0200

    notifications update

--- services/notification.js
+++ services/notification.js
@@ -7,7 +7,13 @@ function notifyMe(message) {
     // Проверка разрешения на отправку уведомлений
     else if (Notification.permission === 'granted' && message) {
       // Если разрешено, то создаем уведомление
-      const notification = new Notification(message);
+      const notification = new Notification('Замовлення готове!', {
+        body: 'Підходь на видачу і забирай :)',
+        icon: 'https://e7.pngegg.com/pngimages/1024/637/png-clipart-youtube-computer-icons-button-small-bell-hat-black-thumbnail.png',
+        vibrate: window.navigator.vibrate([200, 100, 200]),
+        requireInteraction: true,
+        lang: 'ua'
+      });
     }
   
     // В противном случае, запрашиваем разрешение
