39ad417dcfc6929b7f92d8b6830c76a46b81d820
Eugene Butyrin <Jimmydeanmaster@gmail.com>
2021-10-10 12:46:31 +0300

    fab code pixel

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,23 @@ export default function Home() {
 					async
 					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
 				/>
+				<!-- Facebook Pixel Code -->
+				<script>
+				  !function(f,b,e,v,n,t,s)
+				  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+				  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+				  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+				  n.queue=[];t=b.createElement(e);t.async=!0;
+				  t.src=v;s=b.getElementsByTagName(e)[0];
+				  s.parentNode.insertBefore(t,s)}(window, document,'script',
+				  'https://connect.facebook.net/en_US/fbevents.js');
+				  fbq('init', '6105494199525716');
+				  fbq('track', 'PageView');
+				</script>
+				<noscript><img height="1" width="1" style="display:none"
+				  src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_PIXEL}&ev=PageView&noscript=1`}
+				/></noscript>
+				<!-- End Facebook Pixel Code -->
 				<script
 					dangerouslySetInnerHTML={{
 						__html: `
